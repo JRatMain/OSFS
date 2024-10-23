86521b3992b9899a51ed1f96cc0149dec78970e9
Matthew Vrbka <matthewjvrbka@gmail.com>
2024-10-23 13:10:31 -0500

    project creation for OS filesystem team project. some code has been put in to make a UI

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def welcome():
+    dashes = '------------------------'
+    equals = '========================'
+    print(dashes)
+    print(equals)
+    print()
+
+def del_file(args):
+    print()
+
+
+if __name__ == "main":
+    welcome()
