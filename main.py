169ef512073e5e8364e9678d0654c8ee03ab5a84
Matthew Vrbka <matthewjvrbka@gmail.com>
2024-10-23 13:51:51 -0500

    made further changes to the intro and added menu options. also adding function stubs for each option

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 
 def getInput():
-    print('Please enter a number:')
+    prompt = 'Please enter a number: '
+
+    baseChoice = int(input(prompt))
+    if baseChoice == 1:
+        return baseChoice
+    elif baseChoice == 2:
+        return baseChoice
+    elif baseChoice == 3:
+        return baseChoice
+    elif baseChoice == 0:
+        print("Here is a list of commands:")
+        print('1. Create a file.')
+        print('2. Delete a file.')
+        print('3. Show all files.')
+    else:
+        print()
+        print('Invalid selection. If you need a list of commands, enter 0.')
+
+
+
 def welcome():
     dashes = '------------------------'
     equals = '========================'
@@ -17,11 +36,17 @@ def welcome():
     print(dashes)
 
 
-def del_file(args):
-    print()
+def del_file():
+    None
 
+def mk_file():
+    None
 
-if __name__ == "main":
+def show_files():
+    None
+
+if __name__ == "__main__":
     welcome()
-    choice = getInput()
+    while (True):
+        choice = getInput()
 
